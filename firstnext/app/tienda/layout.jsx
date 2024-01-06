2339e9717d193e81a723891e95f4b1c0d9710861
patxi <patxiarana05@gmail.com>
2024-01-06 09:03:01 -0300

    routes y layouts

---
+++ firstnext/app/tienda/layout.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+
+export default function TiendaLayout({ children }) {
+    return <>
+        <h3>Seccion Tienda</h3>
+
+        <ul>
+            <li>
+                <Link href='/tienda/categorias'>Categorias</Link>
+            </li>
+            <li>
+                <Link href='/tienda/categorias/computadoras'>Computadoras</Link>
+            </li>
+        </ul>
+        {children}
+
+    </>
+}
---
+++ firstnext/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+
+
+export default function NavBar() {
+    return (<nav>
+        <h1>NavBar</h1>
+        <ul>
+            <li>
+                <Link href="/">Home</Link></li>
+            <li>
+                <Link href="/about">About</Link>
+            </li>
+            <li>
+                <Link href="/tienda">Tienda</Link>
+            </li>
+        </ul>
+    </nav>)
+}
