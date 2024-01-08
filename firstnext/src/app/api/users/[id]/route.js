117e4b48ace54299ddda2b6598845abc2f0d5894
patxi <patxiarana05@gmail.com>
2024-01-08 16:01:39 -0300

    Search params

--- firstnext/src/app/api/users/[id]/route.js
+++ firstnext/src/app/api/users/[id]/route.js
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request, {params}) {
-    console.log(request)
+ const {searchParams} =   new URL(request.url)
+ console.log(searchParams)
+ console.log(searchParams.get('nombre'))
+ console.log(searchParams.get('apellidos'))
  const res =  await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
 const data =  await res.json()
    return  NextResponse.json(data)
